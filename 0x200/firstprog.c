c0e25e2ed19861ce15f018e15c689f4ec67f3bce
Jobson Batista <jobson2084@gmail.com>
2024-05-01 16:34:48 -0300

    Project organized

---
+++ 0x200/firstprog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    int i;
+    
+    for(i=0; i < 10;i++) {
+        puts("Hello World!\n");
+    }
+
+    return 0; // Diz ao SO que o programa encerrou sem erros
+}
